14e7c8881b35fa104c73c243c321a5cc11cbbc41
Davi0708 <tiklixotiklixo@gmail.com>
2021-05-13 22:33:50 -0300

    First commit

---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 10 / GRUPO (MÊS)*
+╠➥ *CRIAR: 35 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ *MERCADO PAGO, BOLETO,*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/5521999517686*
+║
+╚═〘  DAVIZIN  〙
+`
+}
+exports.iklan = iklan
